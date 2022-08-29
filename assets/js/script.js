988a47858ab9a7e7a5d224371f92b66657138729
mafiahash <danil.puschin61@gmail.com>
2022-08-30 01:12:36 +0300

    123

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,38 @@ $('.nav__burger').on('click', function(e) {
     e.preventDefault();
     $('.mobile__nav').toggleClass('mobile__nav_active');
   })
+
+
+  let header = document.getElementById('header').classList
+  let active_class = "header-active"
+  
+  /**
+   * Слушаем событие прокрутки
+   */
+  window.addEventListener('scroll', e => {
+    if(scrollY > 10) header.add(active_class)
+    else header.remove(active_class)
+  })
+
+  let mobile_header = document.getElementById('header__mobile').classList
+
+
+  window.addEventListener('scroll', e => {
+    if(scrollY > 10) mobile_header.add(active_class)
+    else mobile_header.remove(active_class)
+  })
+
+
+const openPopUp = document.getElementById('open_pop_up')
+const closePopUp = document.getElementById('close_pop_up')
+const PopUp = document.getElementById('pop_up')
+
+openPopUp.addEventListener('click', function(e){
+  e.preventDefault();
+  PopUp.classList.add('popup__active');
+})
+
+closePopUp.addEventListener('click', function(e){
+  e.preventDefault();
+  PopUp.classList.remove('popup__active');
+})
